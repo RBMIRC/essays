b546a54ad0b3f13250c39faae232d6d5c3b8f430
RBMIRC <retconblackmountain@gmail.com>
2026-01-12 20:09:48 +0100

    Add Verified Network visualization, rename Double Helix link

--- quartz/.quartz-cache/transpiled-worker.mjs
+++ quartz/.quartz-cache/transpiled-worker.mjs
@@ -6764,7 +6764,8 @@ var ImagePreview_default = /* @__PURE__ */ __name((() => {
 import { jsx as jsx41, jsxs as jsxs25 } from "preact/jsx-runtime";
 var ExternalLinks = /* @__PURE__ */ __name(() => {
   const links = [
-    { name: "Graph - The Double Helix", url: "/essays/static/bmc-helix/bmc-helix.html" },
+    { name: "The Double Helix", url: "/essays/static/bmc-helix/bmc-helix.html" },
+    { name: "Verified Network", url: "/essays/static/bmc-network/bmc-verified-network.html" },
     { name: "The Dining Hall", url: "https://retconblackmountain.info/" },
     { name: "The Library", url: "https://thelibrary.retconblackmountain.info/" },
     { name: "\u{1F331}", url: "https://www.couzinetjacques.com" }
--- quartz/components/ExternalLinks.tsx
+++ quartz/components/ExternalLinks.tsx
@@ -2,7 +2,8 @@ import { QuartzComponent, QuartzComponentConstructor } from "./types"
 
 const ExternalLinks: QuartzComponent = () => {
   const links = [
-    { name: "Graph - The Double Helix", url: "/essays/static/bmc-helix/bmc-helix.html" },
+    { name: "The Double Helix", url: "/essays/static/bmc-helix/bmc-helix.html" },
+    { name: "Verified Network", url: "/essays/static/bmc-network/bmc-verified-network.html" },
     { name: "The Dining Hall", url: "https://retconblackmountain.info/" },
     { name: "The Library", url: "https://thelibrary.retconblackmountain.info/" },
     { name: "🌱", url: "https://www.couzinetjacques.com" },
